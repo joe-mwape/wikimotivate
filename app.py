d4e697dd0dd2e1384af8bf149b25bb5592cdde0e
joe-mwape <2019090295@student.unza.zm>
2023-11-15 08:56:12 +0200

    commit

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, render_template, request, jsonify
 from subprocess import Popen, PIPE
 from flask_cors import CORS
 
@@ -7,7 +7,7 @@ CORS(app)
 
 @app.route('/')
 def index():
-    return 'Hello, this is your Flask server!'
+    return render_template('index.html')
 
 @app.route('/run-script', methods=['POST'])
 def run_script():
